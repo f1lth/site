2da27b0b55c8c6e53338cf9881618fd301dc97e4
max kang <max@puffdigital.com>
2021-07-06 12:30:39 -0400

    some themeing

--- components/shared/nav/cart/desktop-cart-item.tsx
+++ components/shared/nav/cart/desktop-cart-item.tsx
@@ -1,7 +1,9 @@
-import styled from "styled-components";
+import styled, { withTheme } from "styled-components";
 
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { CheckoutItemFragment } from "api/fragments/checkout-item.graphql";
 import { CloseButton } from "components/shared/svg/close-button";
@@ -27,40 +29,52 @@ export function DesktopCartItem(props: DesktopCartItemProps): JSX.Element {
     costOfCheckoutItem,
   } = props;
 
-  return (
-    <CheckoutItem>
-      <LeftSide>
-        <CheckoutItemImageContainer>
-          <CheckoutItemImage src={item.product.image} />
-        </CheckoutItemImageContainer>
-        <div>
-          <ItemName>{item.product.name}</ItemName>
-        </div>
-      </LeftSide>
+  const useStyles = makeStyles((theme) => ({
+    menu: {
+      "& .MuiPaper-root": {
+        backgroundColor: "lightblue",
+      },
+    },
+  }));
+  const classes = useStyles();
 
-      <RightSide>
-        <QuantitySelect
-          value={item.quantity}
-          onChange={(e) => {
-            handleCheckoutQuantityUpdate(item, e.target.value as number);
-          }}
-          variant="outlined"
-        >
-          {QUANTITIES.map((quantity) => (
-            <MenuItem key={quantity} value={quantity}>
-              {quantity}
-            </MenuItem>
-          ))}
-        </QuantitySelect>
-        <Price>{formatPrice(costOfCheckoutItem(item))}</Price>
-        <CloseButton
-          height={20}
-          width={20}
-          onClick={() => handleRemoveItemFromCheckout(item)}
-          isDark
-        />
-      </RightSide>
-    </CheckoutItem>
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CheckoutItem>
+        <LeftSide>
+          <CheckoutItemImageContainer>
+            <CheckoutItemImage src={item.product.image} />
+          </CheckoutItemImageContainer>
+          <div>
+            <ItemName>{item.product.name}</ItemName>
+          </div>
+        </LeftSide>
+        <Center>
+          <QuantitySelect
+            value={item.quantity}
+            onChange={(e) => {
+              handleCheckoutQuantityUpdate(item, e.target.value as number);
+            }}
+            variant="outlined"
+          >
+            {QUANTITIES.map((quantity) => (
+              <MenuItem key={quantity} value={quantity}>
+                {quantity}
+              </MenuItem>
+            ))}
+          </QuantitySelect>
+        </Center>
+        <RightSide>
+          <Price>{formatPrice(costOfCheckoutItem(item))}</Price>
+          <CloseButton
+            height={20}
+            width={20}
+            onClick={() => handleRemoveItemFromCheckout(item)}
+            isDark
+          />
+        </RightSide>
+      </CheckoutItem>
+    </MuiThemeProvider>
   );
 }
 
@@ -134,3 +148,30 @@ const QuantitySelect = styled(Select)`
     padding: 14px 15px;
   }
 `;
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiInputBase: {
+      root: {
+        color: "#ffffff",
+      },
+    },
+    MuiAccordion: {
+      root: {
+        boxShadow: "none",
+        borderTop: "none",
+        padding: "0px 0px 0px 0px",
+        "&:before": {
+          display: "none",
+        },
+      },
+    },
+    MuiSelect: {
+      icon: {
+        82: {
+          color: "#fffffff",
+        },
+      },
+    },
+  },
+});
